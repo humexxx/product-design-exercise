526a817b8ba48a2e6d7dbb91b02b0309ee423d5c
humexxx <jahume92@gmail.com>
2026-03-12 23:40:14 -0600

    Implement bookmarks feature: add bookmarks list to homepage and update bookmark actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   def home
     @total_posts = Post.count
     @recent_posts = Post.includes(:bookmarks).order(created_at: :desc).limit(3)
+    @bookmarks = Bookmark.includes(:bookmarkable)
+                         .where(bookmarkable_type: "Post")
+                         .order(created_at: :desc)
   end
 end
--- app/controllers/posts/bookmarks_controller.rb
+++ app/controllers/posts/bookmarks_controller.rb
@@ -4,6 +4,7 @@ module Posts
 
     def create
       @bookmark = @post.bookmarks.create!
+      set_homepage_bookmarks
 
       respond_to do |format|
         format.turbo_stream
@@ -13,6 +14,7 @@ module Posts
 
     def destroy
       @post.bookmarks.destroy_all
+      set_homepage_bookmarks
 
       respond_to do |format|
         format.turbo_stream
@@ -24,6 +26,12 @@ module Posts
 
     def set_post
       @post = Post.find(params[:post_id])
+    end
+
+    def set_homepage_bookmarks
+      @bookmarks = Bookmark.includes(:bookmarkable)
+                           .where(bookmarkable_type: "Post")
+                           .order(created_at: :desc)
     end
   end
 end
